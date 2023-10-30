cce4a4b8141adedc8a2ecbf180409c823784a53c
vinu-rgowda <149353807+vinu-rgowda@users.noreply.github.com>
2023-10-30 10:51:22 +0530

    Add files via upload

---
+++ form/script.js
@@ -0,0 +1,19 @@
+function getVal() {
+    let val1=document.getElementById("id1").value;
+    console.log(val1);
+
+    let val2=document.getElementById("id2").value;
+    console.log(val2);
+  
+    let val3=document.getElementById("id3").value;
+    console.log(val3);
+
+    let val4=document.getElementById("id4").value;
+    console.log(val4);
+ 
+    let val5=document.getElementById("id5").value;
+    console.log(val5);
+
+    let val6=document.getElementById("id6").value;
+    console.log(val6);
+  }
